482e2856d61666c02d2278e7f729bda5067d7335
Brandon Jacobs <jacobs27@byu.edu>
2023-02-08 18:31:01 -0700

    Add project files.

---
+++ MIssion6_jacobs27/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MIssion6_jacobs27.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace MIssion6_jacobs27.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Podcasts()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult MovieForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult MovieForm (Movie m)
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MIssion6_jacobs27/Models/Movie.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MIssion6_jacobs27.Models
+{
+    public class Movie
+    {
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        [Range(1000, 9999)]
+        public string Year { get; set; }
+        [Required]
+        public string Director { get; set; }
+        [Required]
+        public string Rating { get; set; }
+        [Required]
+        public string Category { get; set; }
+        [Required]
+        public string LentTo { get; set; }
+        public bool Edited { get; set; }
+        [StringLength(25)]
+        public string Notes { get; set; }
+    }
+}
